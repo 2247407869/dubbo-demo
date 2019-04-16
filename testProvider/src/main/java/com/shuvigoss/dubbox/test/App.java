99448b326fb5e8f3cf9c0228ce88bb0775056a30
lils <2247407869@qq.com>
2019-04-16 14:56:46 +0800

    成功运行dubbo demo

--- testProvider/src/main/java/com/shuvigoss/dubbox/test/App.java
+++ testProvider/src/main/java/com/shuvigoss/dubbox/test/App.java
@@ -1,13 +1,21 @@
 package com.shuvigoss.dubbox.test;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-local.xml",
+                "classpath:spring-services.xml");
+        System.out.println(context.getDisplayName() + ": here");
+        context.start();
+        System.out.println("服务已经启动...");
+        System.in.read();
     }
 }
